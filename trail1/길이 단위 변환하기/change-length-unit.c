adfb00ed318208660f90c462c2717e2116d6c6ce
kjw673 <cooling12321@gmail.com>
2026-09-03 15:53:20 +0900

    [Passed] 길이 단위 변환하기 (1ms, 0MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float ft = 30.48;
+    float mi = 160934;
+
+    printf("9.2ft = %.1fcm",9.2*ft);
+    printf("\n");
+    printf("1.3mi = %.1fcm",1.3*mi);
+    return 0;
+}
