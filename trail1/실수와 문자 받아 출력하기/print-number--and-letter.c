a5d232887e2b055ea188c80986d0e9656f33f396
kjw673 <cooling12321@gmail.com>
2026-09-06 02:34:30 +0900

    [Passed] 실수와 문자 받아 출력하기 (1ms, 0MB)

---
+++ trail1/실수와 문자 받아 출력하기/print-number--and-letter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double a,b;
+    char c;
+    scanf("%c %lf %lf",&c,&a,&b);
+    printf("%c\n%.2lf\n%.2lf",c,a,b);
+    return 0;
+}
