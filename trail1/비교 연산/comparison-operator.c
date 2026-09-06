751025e6e073e60e1af1634e331fd1cac7f09a91
kjw673 <cooling12321@gmail.com>
2026-09-06 18:53:48 +0900

    [Passed] 비교 연산 (1ms, 0MB)

---
+++ trail1/비교 연산/comparison-operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n",a>=b,a>b,b>=a,b>a,a==b,a!=b);
+    return 0;
+}
