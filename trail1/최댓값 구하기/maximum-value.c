66c35055934d4750bd54b5e4aa92a5ac803a13ae
kjw673 <cooling12321@gmail.com>
2026-09-06 20:04:20 +0900

    [Passed] 최댓값 구하기 (1ms, 0MB)

---
+++ trail1/최댓값 구하기/maximum-value.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b){
+        if(a>c){
+            printf("%d",a);
+        }else{
+            printf("%d",c);
+        }
+    }else{
+        if(b>c){
+            printf("%d",b);
+        }else{
+            printf("%d",c);
+        }
+    }
+    return 0;
+}
