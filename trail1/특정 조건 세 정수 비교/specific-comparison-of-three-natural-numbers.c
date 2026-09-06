113d6665eef1941c4fad43f3cd804a1e9acf3931
kjw673 <cooling12321@gmail.com>
2026-09-06 19:15:52 +0900

    [Passed] 특정 조건 세 정수 비교 (1ms, 0MB)

---
+++ trail1/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d %d",a<=b&&a<=c,a==b&&a==c);
+    return 0;
+}
