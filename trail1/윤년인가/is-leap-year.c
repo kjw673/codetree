7f690cb436de0c0843b9a6dc60d51c266580d7f4
kjw673 <cooling12321@gmail.com>
2026-09-06 19:57:41 +0900

    [Passed] 윤년인가 (1ms, 0MB)

---
+++ trail1/윤년인가/is-leap-year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int Y;
+    scanf("%d",&Y);
+    if(Y%4==0){
+        if(Y%100==0 && Y%400!=0){
+            printf("false");
+        }
+        else{
+            printf("true");
+        }
+    }else{
+        printf("false");
+    }
+    return 0;
+}
