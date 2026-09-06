de7530c0d0e6213b72159520b30d50bb51f23003
kjw673 <cooling12321@gmail.com>
2026-09-06 18:11:15 +0900

    [Passed] 2개 중 최대 (1ms, 0MB)

---
+++ trail1/2개 중 최대/max-of-two-nums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>b){
+        printf("%d",a);
+    }
+    else{
+        printf("%d",b);
+    }
+    return 0;
+}
