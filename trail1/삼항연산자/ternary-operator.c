b096329a5cf788eead092026fb3304ed0f7ba8d7
kjw673 <cooling12321@gmail.com>
2026-09-06 18:16:06 +0900

    [Passed] 삼항연산자 (2ms, 0MB)

--- trail1/삼항연산자/ternary-operator.c
+++ trail1/삼항연산자/ternary-operator.c
@@ -4,9 +4,10 @@ int main() {
     // Please write your code here.
     int score;
     scanf("%d",&score);
-    if(score == 100){
+    int pass = score == 100 ? 1 : 0;
+    if(pass == 1){
         printf("pass");
-    }
+    } 
     else{
         printf("failure");
     }
