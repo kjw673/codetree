323540b4742a0ef06b4a88fea117dc712907d24b
kjw673 <cooling12321@gmail.com>
2026-09-09 00:25:14 +0900

    [Passed] N까지 자연수 출력 (1ms, 0MB)

---
+++ trail1/N까지 자연수 출력/print-1-to-n.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    int i = 1;
+    scanf("%d",&n);
+    while(n>=i){
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
