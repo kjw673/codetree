22db82dcde06847afebdf30697a2a31250964ecd
kjw673 <cooling12321@gmail.com>
2026-09-09 14:12:26 +0900

    [Passed] N번 더하기 (2ms, 0MB)

---
+++ trail1/N번 더하기/add-n-times.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,n;
+    scanf("%d %d",&a,&n);
+    for(int i=0;i<n;i++){
+        printf("%d\n",a+n);
+        a+=n;
+    }
+    return 0;
+}
