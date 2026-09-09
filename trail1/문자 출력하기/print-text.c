ab4a37c6b56f60964f556a021595e723901b8d0b
kjw673 <cooling12321@gmail.com>
2026-09-09 13:33:17 +0900

    [Passed] 문자 출력하기 (1ms, 0MB)

---
+++ trail1/문자 출력하기/print-text.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char c;
+    scanf("%c",&c);
+    for(int i=0;i<8;i++){
+        printf("%c",c);
+    }
+    return 0;
+}
