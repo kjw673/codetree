924705de2f1bd2b472f40b9ec0dbf1ac61833d0e
kjw673 <cooling12321@gmail.com>
2026-09-09 13:29:09 +0900

    [Passed] n부터 1까지 정수 출력 (1ms, 0MB)

---
+++ trail1/n부터 1까지 정수 출력/print-n-to-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    while(n>0){
+        printf("%d ",n);
+        n--;
+    }
+    return 0;
+}
