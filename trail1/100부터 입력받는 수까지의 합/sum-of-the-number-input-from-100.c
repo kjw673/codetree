9b1c8061248b48ef2c45a71302bf4263023e54a5
kjw673 <cooling12321@gmail.com>
2026-09-17 01:11:42 +0900

    [Passed] 100부터 입력받는 수까지의 합 (1ms, 0MB)

---
+++ trail1/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int i=n;i<=100;i++){
+        sum+=i;
+    }
+    printf("%d",sum);
+    return 0;
+}
