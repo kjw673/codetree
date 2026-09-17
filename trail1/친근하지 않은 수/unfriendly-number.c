de4b4b8d0f0ca976a73d59e539958bd18421369f
kjw673 <cooling12321@gmail.com>
2026-09-17 15:45:15 +0900

    [Passed] 친근하지 않은 수 (1ms, 0MB)

---
+++ trail1/친근하지 않은 수/unfriendly-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n,cnt=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(i%2==0 || i%3==0 || i%5==0){
+            continue;
+        }
+        cnt++;
+    }
+
+    printf("%d",cnt);
+    return 0;
+}
