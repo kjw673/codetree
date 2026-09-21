2feb83b27738887373b45173a2a4e48c7f4af695
kjw673 <cooling12321@gmail.com>
2026-09-21 15:19:12 +0900

    [Passed] 별 그리기 (1ms, 0MB)

---
+++ trail1/별 그리기/star-drawing.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){ 
+        for(int k=0;k<n-i;k++){
+            printf(" ");
+        }
+
+        for(int j=0;j<2*i-1;j++){
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for(int i=1;i<n;i++){
+        for(int k=0;k<i;k++){
+            printf(" ");
+        }
+
+        for(int j=0;j<2*(n-i)-1;j++){
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
