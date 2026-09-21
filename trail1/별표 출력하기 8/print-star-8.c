1569962e62e67617cca11fea32195c1752856104
kjw673 <cooling12321@gmail.com>
2026-09-21 15:35:26 +0900

    [Passed] 별표 출력하기 8 (1ms, 0MB)

---
+++ trail1/별표 출력하기 8/print-star-8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        if(i%2==1) printf("*\n");
+
+        if(i%2==0){
+            for(int j=0;j<i;j++){
+                printf("* ");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
