2262d92c83dd6ded7cf7413c7215a3598ecc0e7b
kjw673 <cooling12321@gmail.com>
2026-09-21 15:27:46 +0900

    [Passed] 체크 다이아몬드 (2ms, 0MB)

---
+++ trail1/체크 다이아몬드/check-diamond.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n-i;j++){
+            printf(" ");
+        }
+
+        for(int k=0;k<i;k++){
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            printf(" ");
+        }
+
+        for(int k=0;k<n-i;k++){
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
