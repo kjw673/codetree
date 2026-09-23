45a1115be996745488e3f2dcb94e8dbba7df8f8c
kjw673 <cooling12321@gmail.com>
2026-09-23 15:02:16 +0900

    [Passed] 별표 출력하기 12 (2ms, 0MB)

---
+++ trail1/별표 출력하기 12/print-star-12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0){
+                printf("* ");
+            }
+            else if(j%2==0){
+                printf("  ");
+            }else{
+                if(j>=i){
+                    printf("* ");
+                }else{
+                    printf("  ");
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
